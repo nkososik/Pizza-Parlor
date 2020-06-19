a3becdea4cef50a14f8524910f51653b08964030
Nikolas Kososik <nikolas.kososik@gmail.com>
2020-06-19 10:32:47 -0700

    add protoype to push toppings to an array in the pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,15 @@ function Order() {
   this.orders = [];
 }
 
+// Pizza Constructor and Prototypes
 function Pizza() {
   this.toppings = [];
   this.size = size;
 }
+
+Pizza.prototype.toppingsAdded = function(element) {
+  this.toppings.push(element);
+}
+
 
 // User Interface Logic
